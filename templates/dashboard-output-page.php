cd10309a367fbdbdb18b7d684b47c1e47f34998d
Jeromedia <info@jeromedia.com>
2025-04-11 16:27:40 -0500

    Reorganize dashboard output layout and display API base URL and active company name

--- templates/dashboard-output-page.php
+++ templates/dashboard-output-page.php
@@ -2,6 +2,13 @@
     <h1>JM LinkedIn Posts - Dashboard</h1>
     <p>Welcome to the dashboard of JM LinkedIn Posts.</p>
     <div>
-       <?php require_once JM_LI_PLUGIN_PATH . '/templates/' . 'shortcode-output-page.php'; ?>
+        <br>
+        base_url: <?php echo jm_li_get_api_base_url(); ?><br>
+        company: <?php echo jm_li_get_active_company_name(); ?><br>
+        url: <?php echo "$base_url/posts/$company"; ?><br>
+        </div>
+
+
+        <?php require_once JM_LI_PLUGIN_PATH . '/templates/' . 'shortcode-output-page.php'; ?>
     </div>
 </div>
