83b23c58bddbb4a64b48b58e3c48c0374a078535
Jeromedia <info@jeromedia.com>
2025-04-13 15:36:26 -0500

    Fix variable name for connection response in API page function

--- includes/api.php
+++ includes/api.php
@@ -5,12 +5,12 @@ if (!defined('ABSPATH')) {
 
 function jm_linkedin_api_page()
 {
-    $connection = jm_li_fetch_connection_to_jeromedia();
+    $response = jm_li_fetch_connection_to_jeromedia();
     // $connection = jm_li_fetch_connection_cached();
     $logo = jm_li_fetch_logo_cached();
     $company = jm_li_get_active_company_name();
     $base_url = jm_li_get_api_base_url();
-    // include plugin_dir_path(__FILE__) . '../templates/api-output-page.php';
+    include plugin_dir_path(__FILE__) . '../templates/api-output-page.php';
     $page = 'api';
     include plugin_dir_path(__FILE__) . '../templates/layouts/main.php';
 }
